09853adaf6988d4672f2c8d373320b516cec3232
Arnob  Chowdhury <arnobchaudhury@gmail.com>
2023-11-14 15:13:06 +0100

    ml setup prtools

--- src/untitled2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 14 15:04:21 2023
-
-@author: arnobchowdhury
-"""
-
---
+++ src/week1/untitled3.py
@@ -0,0 +1,9 @@
+import numpy as np
+import prtools as pr
+
+import matplotlib.pyplot as plt
+
+
+a = pr.gendatb()
+b = +a
+print(b)
